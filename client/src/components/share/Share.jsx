62f4652dcba783540a73ec638c63bace48b85a5d
cognaiger <100193663+cognaiger@users.noreply.github.com>
2023-07-07 11:33:31 +0700

    add file and preview client side

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -2,7 +2,7 @@ import "./share.scss";
 import Image from "../../assets/img.png";
 import Map from "../../assets/map.png";
 import Friend from "../../assets/friend.png";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/authContext";
 import axios from "axios";
 
@@ -11,6 +11,46 @@ const Share = ({posts, setPosts}) => {
     const {currentUser} = useContext(AuthContext);
 
     const [share, setShare] = useState('');
+    const [file, setFile] = useState(null);
+    const [filedata, setFileData] = useState(null);
+    const [errMsg, setErrMsg] = useState('');
+    const [isErr, setIsErr] = useState(false);                   // handle right type upload
+
+    useEffect(() => {
+      let fileReader, isCancel = false;
+      if (file) {
+        fileReader = new FileReader();
+        fileReader.onload = (e) => {
+          const { result } = e.target;
+          if (result && !isCancel) {
+            setFileData(result);
+          }
+        }
+        fileReader.readAsDataURL(file);
+      }
+
+      return () => {
+        isCancel = true;
+        if (fileReader && fileReader.readyState === 1) {
+          fileReader.abort();
+        }
+      }
+
+    }, [file])
+
+    const handleFileChange = (e) => {
+      const selectedFile = e.target.files[0];
+
+      const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
+      if (!allowedTypes.includes(selectedFile?.type)) {
+        setIsErr(true);
+        setErrMsg("Only jpeg, png, gif are allowed");
+        return;
+      }
+
+      setIsErr(false);
+      setFile(selectedFile);
+    }
 
     async function getPost(id) {
       const response = await axios.get(`http://localhost:2504/post/${id}`);
@@ -27,7 +67,8 @@ const Share = ({posts, setPosts}) => {
 
         const response = await axios.post("http://localhost:2504/post/create", {
             description: share,
-            userId: currentUser.id
+            userId: currentUser.id,
+            file: file
         })
 
         if (response.status === 201) {
@@ -55,10 +96,15 @@ const Share = ({posts, setPosts}) => {
           <input type="text" placeholder={`What's on your mind ${currentUser.name}?`} 
           value={share} onChange={(e) => setShare(e.target.value)}/>
         </div>
+        {filedata && 
+        <div className="image-wrapper">
+          <img className="image-preview" src={filedata} alt="preview" />
+        </div>
+        }
         <hr />
         <div className="bottom">
           <div className="left">
-            <input type="file" id="file" style={{display:"none"}} />
+            <input type="file" id="file" style={{display:"none"}} onChange={handleFileChange} />
             <label htmlFor="file">
               <div className="item">
                 <img src={Image} alt="" />
@@ -78,6 +124,7 @@ const Share = ({posts, setPosts}) => {
             <button onClick={handleShare}>Share</button>
           </div>
         </div>
+        {isErr && <p>{errMsg}</p>}
       </div>
     </div>
   );
